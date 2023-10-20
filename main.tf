48361187a0e05db75fb5b8bcd34a57cbfb529012
samiha <samihauddin@live.co.uk>
2023-10-20 10:40:16 +0100

    main

---
+++ main.tf
@@ -0,0 +1,19 @@
+# Who is the cloud provider 
+provider "aws" {
+
+
+# location of aws
+  region = "eu-west-1"
+}
+# to download required dependencies 
+# create a service/resource on the cloud - ec2 on aws
+
+resource "aws_instance" "samiha-iac-test" {
+   ami = "ami-0943382e114f188e8"
+   instance_type = "t2.micro"
+   tags = {
+     Name = "samiha-iac-tf-test"
+   }
+
+}
+
